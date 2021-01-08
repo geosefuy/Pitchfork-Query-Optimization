cb6875ccdf18f55ab12932098e6dddaa36a5c012
Jason Evangelista <jase_evangelista@dlsu.edu.ph>
2021-01-08 14:12:23 +0800

    commented out normalized db queries

--- routes/two_table.js
+++ routes/two_table.js
@@ -17,22 +17,23 @@ module.exports = {
             year1 = year2;
             year2 = dum;
         }
-        // let query = `SELECT r.author, COUNT(r.author) reviewCount
-        //                 FROM reviews r, years y
-        //                 WHERE r.reviewid = y.reviewid AND
-        //                     y.year BETWEEN ` + year1 + ` AND ` + year2 + `
-        //                 GROUP BY r.author
-        //                 ORDER BY COUNT(r.author) DESC
-        //                 LIMIT 3`;
-        let query = `SELECT a.author, COUNT(a.author) reviewCount
-                        FROM reviews r, years y, authors a
+        let query = `SELECT r.author, COUNT(r.author) reviewCount
+                        FROM reviews r, years y
                         WHERE r.reviewid = y.reviewid AND
-                            a.authorid = r.authorid 
-                            AND
                             y.year BETWEEN ` + year1 + ` AND ` + year2 + `
-                        GROUP BY a.author
-                        ORDER BY COUNT(a.author) DESC
+                        GROUP BY r.author
+                        ORDER BY COUNT(r.author) DESC
                         LIMIT 3`;
+        // normalized query
+        // let query = `SELECT a.author, COUNT(a.author) reviewCount
+        //                 FROM reviews r, years y, authors a
+        //                 WHERE r.reviewid = y.reviewid AND
+        //                     a.authorid = r.authorid 
+        //                     AND
+        //                     y.year BETWEEN ` + year1 + ` AND ` + year2 + `
+        //                 GROUP BY a.author
+        //                 ORDER BY COUNT(a.author) DESC
+        //                 LIMIT 3`;
         console.log(query);
         let t0 = performance.now();
         db.query(query, (err, output) => {
